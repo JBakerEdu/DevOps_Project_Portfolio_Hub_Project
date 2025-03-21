1e873bb3a0513df7d6d360333d534d0b5438b8c3
Jacob Baker <jb00645@my.westga.edu>
2025-03-20 22:09:55 -0400

    Added the UI for Login and Create Account
    Created only the UI for the Login and the Create Account Pages.

---
+++ code/Project_Portfolio/Project_Portfolio/src/main/java/edu/westga/comp4420/project_portfolio/view/codebehind/CreateAccountPageView.java
@@ -0,0 +1,76 @@
+package edu.westga.comp4420.project_portfolio.view.codebehind;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * CodeBehind To Handle Processing for the Create Account Page
+ *
+ * @author	Jacob Baker
+ * @version Spring 2025
+ */
+public class CreateAccountPageView {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private PasswordField confirmPasswordTextFeild;
+
+    @FXML
+    private TextField emailTextFeild;
+
+    @FXML
+    private Label errorNotConfirmedPassword;
+
+    @FXML
+    private Label errorNotCorrectPassword;
+
+    @FXML
+    private Label errorNotValidEmail;
+
+    @FXML
+    private Label errorNotValidUsername;
+
+    @FXML
+    private PasswordField passwordTextFeild;
+
+    @FXML
+    private TextField userNameTextFeild;
+
+    @FXML
+    void handleCreateAccountButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHasAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleHomeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handlePersonalAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleSearchBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSearchButtoneClick(MouseEvent event) {
+
+    }
+
+}
---
+++ code/Project_Portfolio/Project_Portfolio/src/main/java/edu/westga/comp4420/project_portfolio/view/codebehind/LoginPageView.java
@@ -0,0 +1,64 @@
+package edu.westga.comp4420.project_portfolio.view.codebehind;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * CodeBehind To Handle Processing for the Login Page
+ *
+ * @author	Jacob Baker
+ * @version Spring 2025
+ */
+public class LoginPageView {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Label errorNotCorrectPassword;
+
+    @FXML
+    private Label errorNotValidUsername;
+
+    @FXML
+    private PasswordField passwordTextFeild;
+
+    @FXML
+    private TextField userNameTextFeild;
+
+    @FXML
+    void handleHomeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleLoginButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNoAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handlePersonalAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleSearchBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSearchButtoneClick(MouseEvent event) {
+
+    }
+
+}
